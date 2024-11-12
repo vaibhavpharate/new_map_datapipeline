49e36c80d96d62e1258c4962e3cf59fd2b4b8efa
vaibhavpharate <vaibhavpharate999@gmail.com>
2024-11-12 17:18:30 +0530

    transfer_different

--- transfer_forecast.py
+++ transfer_forecast.py
@@ -94,7 +94,13 @@ def get_exim_files(ssh_client,usa_date):
 
 
 def transfer_exim_files(ssh_client,usa_date,timestamp,file_name,forecast_timestamp,variable,db_connection,data_connection):
-    sftp_client = ssh_client.open_sftp()
+    try:
+        sftp_client = ssh_client.open_sftp()
+    except Exception as e:
+        print("CANNOT GET SSH CONNECTION")
+        ssh_client = get_ssh()
+        sftp_client = ssh_client.open_sftp()
+        
     usa_timestamp = timestamp - timedelta(hours=5,minutes=30)
     # usa_time_text = usa_timestamp.strftime(file_timestamp_format)
     tmstp = timestamp.strftime(file_timestamp_format)
@@ -127,6 +133,7 @@ def transfer_exim_files(ssh_client,usa_date,timestamp,file_name,forecast_timesta
                         print("UPDATING prev forecasst")
                         conn.execute(text(f"UPDATE data_forecast.{variable.lower()} SET st='o' WHERE timestamp='{forecast_timestamp}'"))
                         conn.commit()
+                        conn.close()
                 elif updated_source == timestamp:
                     print("Latest source forecast already in place")
                     return timestamp
@@ -152,11 +159,62 @@ def transfer_exim_files(ssh_client,usa_date,timestamp,file_name,forecast_timesta
     except Exception as e:
         print("Error at EXIM transfer files")
         print(e)
-    finally:
-        ssh_client.close()
+    # finally:
+    #     ssh_client.close()
+
+def read_exim_files(usa_date,timestamp,file_name,forecast_timestamp,variable,db_connection,data_connection):
+    # try:
+    #     sftp_client = ssh_client.open_sftp()
+    # except Exception as e:
+    #     print("CANNOT GET SSH CONNECTION")
+    #     ssh_client = get_ssh()
+    #     sftp_client = ssh_client.open_sftp()
+        
+    usa_timestamp = timestamp - timedelta(hours=5,minutes=30)
+    # usa_time_text = usa_timestamp.strftime(file_timestamp_format)
+    tmstp = timestamp.strftime(file_timestamp_format)
+    fcst_tmstp = forecast_timestamp.strftime(file_timestamp_format)
+    if os.path.exists(f'{destination_path}/EXIM/{usa_date}') == False:
+        os.mkdir(f'{destination_path}/EXIM/{usa_date}')
+
+    try:
+            ## forecast_time search with old_source timestamp if old_source < new_source delete the forecast
+        last_forecast_update = pd.read_sql_query(f"SELECT * FROM files_map_logs.forecast_logs WHERE fcst_timestamp = '{forecast_timestamp}' and variable = '{variable}' order by source_time desc limit 1",con=db_connection)
+        print(last_forecast_update)
+        if len(last_forecast_update)>0:
+                updated_source = list(last_forecast_update['source_time'])[0]
+                if updated_source < timestamp:
+                    ## delete the forecast table
+                    with data_connection.connect() as conn:
+                        print("UPDATING prev forecasst")
+                        conn.execute(text(f"UPDATE data_forecast.{variable.lower()} SET st='o' WHERE timestamp='{forecast_timestamp}'"))
+                        conn.commit()
+                        conn.close()
+                elif updated_source == timestamp:
+                    print("Latest source forecast already in place")
+                    return timestamp
+            
+        file_path = os.path.join(destination_path_exim,usa_date,file_name)
+        data = nc.Dataset(file_path)
+        df = pd.DataFrame()
+        df['lat'] = np.array(data.variables['lat'][:]).flatten()
+        df['lon'] = np.array(data.variables['lon'][:]).flatten()
+        df['timestamp'] = forecast_timestamp
+        
+        for i in variable_atts[variable]:
+                fill_value = data.variables[i].getncattr('_FillValue') if '_FillValue' in data.variables[i].ncattrs() else None
+                df[i] = np.array(data.variables[i][:]).flatten()
+                df.loc[df[i]==fill_value,i] = None
+                df['st'] = 'n'
+                
+        return df
+    except Exception as e:
+        print("Error at EXIM transfer files")
+        print(e)
+
     
 
-# ssh_client = get_ssh()
+#ssh_client = get_ssh()
 send_df = pd.DataFrame()
 
             
@@ -182,7 +240,7 @@ def process_timestamp(row):
                                 port=data_send['port'])  # Data connection
 
     # Transfer files and get dataframe
-    df = transfer_exim_files(ssh_client=ssh_client,
+    df = read_exim_files(
                              usa_date=usa_date,
                              timestamp=row['timestamp'],
                              forecast_timestamp=row['forecasted_for'],
@@ -205,15 +263,17 @@ def process_timestamp(row):
                   con=data_connection,
                   method='multi',
                   chunksize=100000)
-    if resp:
-            
-        df_db = pd.DataFrame({'fcst_timestamp':[row['forecasted_for']],'variable':[row['variable']],'source_time':[row['timestamp']],
-                                          'log_ts':[datetime.now()],'file':[row['file']],'read_status':[1]})
-        df_db.to_sql(schema=file_logs_schema,name='forecast_logs',if_exists='append',con=db_connection,index=False)
-        with data_connection.connect() as conn:
-            conn.execute(text(f"DELETE FROM data_forecast.ct WHERE timestamp='{row['forecasted_for']}' and st='o'"))
-            conn.commit()
-            print("DELETING old timestamps")
+        if resp:
+                
+            df_db = pd.DataFrame({'fcst_timestamp':[row['forecasted_for']],'variable':[row['variable']],'source_time':[row['timestamp']],
+                                            'log_ts':[datetime.now()],'file':[row['file']],'read_status':[1]})
+            df_db.to_sql(schema=file_logs_schema,name='forecast_logs',if_exists='append',con=db_connection,index=False)
+            with data_connection.connect() as conn:
+                conn.execute(text(f"DELETE FROM data_forecast.ct WHERE timestamp='{row['forecasted_for']}' and st='o'"))
+                conn.commit()
+                conn.close()
+                print("DELETING old timestamps")
+        ssh_client.close()
 
 
 if __name__ == "__main__":
@@ -270,6 +330,16 @@ if __name__ == "__main__":
         if len(exim_files)> 0:
             target_files = exim_files.loc[((exim_files['timestamp']>=latest_timestamp)&(exim_files['forecasted_for']<=forecast_end)),:]
             target_files = target_files.sort_values('forecasted_for',ascending=False)
+            for index,_tf, row_tf in target_files.iterrows():
+                transfer_exim_files(ssh_client=ssh_client,
+                                    usa_date=usa_date,
+                                    timestamp=row_tf['timestamp'],
+                                    forecast_timestamp=row_tf['forecasted_for'],
+                                    variable=var,
+                                    file_name=row_tf['file'],
+                                    db_connection=db_connection,
+                                    data_connection=data_connection)
+                
             pool_len = target_files['forecasted_for'].nunique()
             if pool_len > 1:
                 print(pool_len)
